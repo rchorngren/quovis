50c480a3ff5768433a2d8de6ee92b480ef883630
Robert Horngren <robert.horngren@student.kyh.se>
2017-04-06 14:45:59 +0200

    Bootstrap
    Enabled

--- quovis/src/App.js
+++ quovis/src/App.js
@@ -9,10 +9,10 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Quovis</h1>
+          <
         </div>
         
         <p className="App-intro">
-          <div className="well">Skriver något i denna</div>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
