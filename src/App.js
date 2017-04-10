3ec07e55aa21deabbfd70606225be227092cabb1
therealDaze <danieldahlman@outlook.com>
2017-04-10 10:21:44 +0200

    fixed programming enviroment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 
 export default class App extends Component {
+  constructor() {
+    super()
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Quovis</h1>
-        </div>
+      <div className="well">
 
-        <p className="App-intro">
-
-          <div className="well">Skriver något i denna</div>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
