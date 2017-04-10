8b4d4f4c2f016463cac7fe9d8ba0ad6350c7b0e4
therealDaze <danieldahlman@outlook.com>
2017-04-10 10:05:10 +0200

    following codestandard export default class...

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
@@ -12,6 +12,7 @@ class App extends Component {
         </div>
 
         <p className="App-intro">
+
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
@@ -19,7 +20,7 @@ class App extends Component {
   }
 }
 
-export default App;
+
 
 
 
