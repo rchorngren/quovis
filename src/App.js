fbc97692016b17ef08f8079996863187c70b66ae
Nora Arhzane <nora.arhzane@student.kyh.se>
2017-04-18 13:38:09 +0200

    none

--- src/App.js
+++ src/App.js
@@ -8,16 +8,17 @@ import { Link } from 'react-router';
 export default class App extends Component {
 
   render() {
-    return (
-      <div className="well">
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/asdf">Bad link</Link>
-        {this.props.children}
-      </div>
-    );
-  }
+        return (
+          <div className="well">
+            <Link to="/">Home</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/asdf">Bad link</Link>
+          {this.props.children}
+        </div>
+        );
+    }
 }
+
 
 
 
