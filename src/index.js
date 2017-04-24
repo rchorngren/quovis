74d64c321a7dbc5f98a00fbece522aae5ca2af20
therealDaze <danieldahlman@outlook.com>
2017-04-24 13:53:00 +0200

    removed npm url path thingy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { browserHistory } from 'react-router';
-import { URLProvider } from 'react-url';
 import Routes from './routes.js';
 import './index.css';
 
 
 
 
-ReactDOM.render(
-    <Routes history={browserHistory} />,
-    document.getElementById('root')
-);
+ReactDOM.render(<Routes history={browserHistory} />,
+    document.getElementById('root'));
