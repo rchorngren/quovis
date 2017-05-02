14871ee9440dbde0d4d43ddfe65064b882a4a04d
Nora Arhzane <nora.arhzane@student.kyh.se>
2017-05-02 14:15:26 +0200

    closed tag

--- src/api/config.php
+++ src/api/config.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 header('Access-Control-Allow-Origin: http://localhost:3000');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
@@ -6,13 +5,3 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Content-Type: application/json');
 
 $db = mysqli_connect('localhost', 'root', 'root', 'quovis');
-=======
-<?php
-header('Access-Control-Allow-Origin: http://localhost:3000');
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-header('Content-Type: application/json');
-
-$db = mysqli_connect('localhost', 'root', 'root', 'quovis');
->>>>>>> 6f3a7eb7b3010c12833528c3b8b3b8cd1646b78f
-mysqli_query($db, "SET NAMES utf8");
